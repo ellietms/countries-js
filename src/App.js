77644e4bc5cadfaf5e3207fa77fd31b15c6b7e48
ellie <ellie.tms1371@gmail.com>
2020-06-26 22:27:49 +0100

    add drop down

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React,{useState
-} from 'react';
+import React,{useState} from 'react';
 import './App.css';
+import Data from './data.json';
 import Header from './header';
 import DropDown from './DropDown';
 import Countries from './Countries';
 
 function App() {
   const [value,setValue]=useState("")
-  
+
+
   function inputValue(event){
   setValue(event.target.value)
   }
 
+  function showSpecificRegion(countryRegion){
+    Data.filter(country => country.region === countryRegion ? true : false)
+  }
 
   return (
     <div>
       <Header
        value={value}
        setValue={inputValue}/>
-      <DropDown/>
+      <DropDown
+      showSpecificRegion={showSpecificRegion}/>
       <Countries/>
     </div>
   );
--- src/DropDown.js
+++ src/DropDown.js
@@ -1,17 +1,36 @@
 import React from 'react';
 
 
-const DropDown = () => {
+const DropDown = ({showSpecificRegion}) => {
     return(
     <div>
     <select name="continent" id="continent">
     <option value="All Region">Filtered by Region</option> 
-    <option value="All Region">All Region</option>
-    <option value="Africa">Africa</option>
-    <option value="America">America</option>
-    <option value="Asia">Asia</option>
-    <option value="Asia">Europe</option>
-    <option value="Asia">Oceania</option>
+    <option 
+    value="All Region"
+    onClick={() => showSpecificRegion("")}>
+        All Region
+    </option>
+    <option value="Africa"
+    onClick={() => showSpecificRegion("Africa")}>
+        Africa
+    </option>
+    <option value="America"
+    onClick={() => showSpecificRegion("Americas")}>
+        America
+    </option>
+    <option value="Asia"
+    onClick={() => showSpecificRegion("Asia")}>
+        Asia
+    </option>
+    <option value="Europe"
+    onClick={() => showSpecificRegion("Europe")}>
+        Europe
+    </option>
+    <option value="Oceania"
+    onClick={() => showSpecificRegion("Oceania")}>
+        Oceania
+    </option>
     </select>
     </div>
     )

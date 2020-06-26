7189a5a1ab26674938bfe223b437824c02ecd57b
ellie <ellie.tms1371@gmail.com>
2020-06-26 22:12:57 +0100

    add drop down

--- src/DropDown.js
+++ src/DropDown.js
@@ -1,1 +1,22 @@
 import React from 'react';
+
+
+const DropDown = () => {
+    return(
+    <div>
+    <select name="continent" id="continent">
+    <option value="All Region">Filtered by Region</option> 
+    <option value="All Region">All Region</option>
+    <option value="Africa">Africa</option>
+    <option value="America">America</option>
+    <option value="Asia">Asia</option>
+    <option value="Asia">Europe</option>
+    <option value="Asia">Oceania</option>
+    </select>
+    </div>
+    )
+}
+
+
+
+export default DropDown;

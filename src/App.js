50feb1041e7ec1164daeccbd724ed0dfeef19377
Daniel Wagner-Hall <dawagner@gmail.com>
2020-06-28 13:26:34 +0100

    Review comments

--- src/App.js
+++ src/App.js
@@ -8,32 +8,32 @@ import CountryInfo from './CountryInfo';
 
 function App() {
 
-  const[inputValue,setInputValue]=useState("");
+  const[inputValue,setInputValue]=useState(""); // It's not obvious from this variable's name how it will be used, can you think of a more descriptive name?
   const[countryRegion,setCountryRegion]=useState(null);
   const[selectedCountryData,setSelectedCountryData]=useState(null);
   
 
-  const filteredCountries = Data.filter((eachCountry) => 
+  const filteredCountries = Data.filter((eachCountry) => // each is a kind of unusual prefix to a variable name - why not just `country`?
    eachCountry.region === countryRegion || 
     countryRegion === null)
   .filter((eachCountry) => 
-    inputValue === "" ? true :
-    eachCountry.name.toLowerCase().includes(inputValue) || 
+    inputValue === "" ? true : // Above you use `||` but here you use `? true :` - both are technically right, but I'd probably try to use the same one (probably the || version). When reading code, seeing two things be the same makes me not need to think about them ("Oh, they're doing the same thing"), whereas seeing things written differently makes me spend time to understand them and think about how and why they're different.
+    eachCountry.name.toLowerCase().includes(inputValue) || // What happens if I type "Iran" in the search box? What _should_ happen?
     eachCountry.capital.toLowerCase().includes(inputValue)
   )
 
-  let BordersOFSelectedCountryData,mainContent;
+  let BordersOFSelectedCountryData,mainContent; // BordersOFSelectedCountryData is only used in one of the `if` branches, so you should only declare it in that one.
 
   if(selectedCountryData === null){
   mainContent=(
   <div>
     <Search
-     inputValueOfSearch={(event) =>setInputValue(event.target.value)}/>
+     inputValueOfSearch={(event) =>setInputValue(event.target.value)}/> { /* It's a little strange for App to know that this function will be used as an event handler, and to know the structure of events it expects. Maybe the other component has links you can click too, or something. I'd expect this prop to be a function which you call with the string being searched for, and for the Search component to translate the event into a string. */ }
     <DropDown
     showSpecificRegion={(region) => setCountryRegion(region)}/>
      <Countries
       allData={filteredCountries}
-      selectedCountryData={(eachCountry) => setSelectedCountryData(eachCountry)}/>
+      selectedCountryData={(eachCountry) => setSelectedCountryData(eachCountry)}/> { /* Names like "selected" sound like they describe something which _already_ happened (like handling an evet which happened) - if the function actively _does_ the thing, I'd call it select* rather than selected*. But maybe I'd call this `showCountry`? */} { /* What's the reason for passing a wrapper arrow function here, rather than just passing setSelectedCountryData? */ }
   </div>
   )}
 
@@ -47,8 +47,8 @@ function App() {
     <CountryInfo 
     selectedCountryData ={selectedCountryData}
     BordersOFSelectedCountryData={BordersOFSelectedCountryData}
-    selectedBorder ={(eachCountry) => setSelectedCountryData(eachCountry)}
-    backToMAinPage={(eachCountry) => setSelectedCountryData(null)}/>
+    selectedBorder ={(eachCountry) => setSelectedCountryData(eachCountry) /* Why the wrapper arrow function? */ }
+    backToMAinPage={(eachCountry) => setSelectedCountryData(null)}/ /* Why does this function take an argument it doesn't use? */ /* The casing of this prop name is unusual/surprising */>
     )
   }
 
--- src/Countries.js
+++ src/Countries.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 const Countries = ({allData,selectedCountryData}) => {
     return(
-        allData.map((eachCountry) => {
+        allData.map((eachCountry) => { /* I would probably extract another component, called something like CountryCard, to show for each country - each country is a useful stand-alone thing to display. After that, I may not bother with this Countries component - just having allData.map(<Country country={country} />) seems fine in App.js :) */
         return(
             <div>
                 <img src={eachCountry.flag}
@@ -13,7 +13,7 @@ const Countries = ({allData,selectedCountryData}) => {
                 style={{width:"100px"}}
                 onClick={() => selectedCountryData(eachCountry)}/>
                 {eachCountry.name},
-                {eachCountry.population.toString().replace(/(.)(?=(\d{3})+$)/g, "$1,")},
+                {eachCountry.population.toString().replace(/(.)(?=(\d{3})+$)/g, "$1,")}, { /* This regular expression is kind of hard to read. Also, in some countries it's wrong (e.g. in Germany they use .s to separate 000s not ,s). Is there a way of doing this which doesn't involve regular expressions? */}
                 {eachCountry.region},
                 {eachCountry.capital},
             </div>

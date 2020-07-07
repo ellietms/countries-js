cbfe72b64be6b8e52c81a7df6bfcb1177f91120a
Daniel Wagner-Hall <dawagner@gmail.com>
2020-07-07 23:53:02 +0100

    Comments

--- src/Countries.js
+++ src/Countries.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Countries = ({ allData, selectedCountryData }) => {
+const Countries = ({ allData, selectedCountryData }) => { // I would call this selectCountry or setSelectedCountry, not selectedCountryData - the past tense makes things sound like they're handling something which already happened.
   return allData.map((eachCountry) => {
     return (
       <div>
@@ -12,7 +12,7 @@ const Countries = ({ allData, selectedCountryData }) => {
             selectedCountryData(eachCountry)}}
         />
         {eachCountry.name},
-        {eachCountry.population.toString().replace(/(.)(?=(\d{3})+$)/g, "$1,")},
+        {eachCountry.population.toString().replace(/(.)(?=(\d{3})+$)/g, "$1,")}, { /* This regular expression is kind of hard to read. Also, in some countries it's wrong (e.g. in Germany they use .s to separate 000s not ,s). Is there a way of doing this which doesn't involve regular expressions? */}
         {eachCountry.region},{eachCountry.capital},
       </div>
     );
--- src/DropDown.js
+++ src/DropDown.js
@@ -6,7 +6,7 @@ const DropDown = ({showSpecificRegion}) => {
       <select onChange={(event) => showSpecificRegion(event.target.value)}>
         {/* fix this  value for all region */}
         <option value=""> All Region</option>
-        <option value="Africa">Africa</option>
+        <option value="Africa">Africa</option> { /* If our data changes to have a new region (let's say it splits Americas into North America and South America), you would need to change this code. Can you make this dropdown in such a way that it shows regions based on what's in the data, not based on what you saw in the data? */ }
         <option value="Americas">America</option>
         <option value="Asia">Asia</option>
         <option value="Europe">Europe</option>
